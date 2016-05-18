56c9e90512b532bd2caa76285615677e18777a55
Byungcheon Ko <bcko@umich.edu>
2016-05-18 13:41:42 -0400

    update

--- script.R
+++ script.R
@@ -1,8 +1,26 @@
-library("foreign")
-dataset1=read.spss("Data/Winter16_FM1.sav", to.data.frame=TRUE)
-dataset2=read.spss("Data/Winter16_FM2.sav", to.data.frame=TRUE)
 
-d1_hmean_t <- t.test(dataset1$h_dialmean ~ dataset1$nation)
+# first set your working directory to dissertation folder
+setwd()
+
+# foreign library needed in order to read spss data
+library("foreign")
+fm1=read.spss("Data/Winter16_FM1.sav", to.data.frame=TRUE)
+
+# return dimension of dataset1
+dim(fm1)
+
+
+summary(fm1$nation) # summary of nation
+
+
+summary(fm1$nation)[["american"]] # number of americans
+
+# number of japanese
+summary(fm1$nation)[["japanese"]]
+
+# Welch's two Sample t-test
+d1_hmean_t <- t.test(fm1$h_dialmean ~ fm1$nation)
+
 d1_hmean_t$p.value
 d1_hmean_t$parameter
 d1_hmean_t$estimate
