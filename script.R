22ca5bd3cc1f64754a3ff343c3e12bfe74e7b65c
Byungcheon Ko <bcko@umich.edu>
2016-05-18 13:57:15 -0400

    update script

--- script.R
+++ script.R
@@ -1,30 +1,38 @@
 
-# first set your working directory to dissertation folder
-setwd()
 
-# foreign library needed in order to read spss data
-library("foreign")
+setwd("C:/Users/TEMP/Documents/MarkDownDissertation") # first set your working directory to dissertation folder
+
+
+library("foreign")	 # foreign library needed in order to read spss data
 fm1=read.spss("Data/Winter16_FM1.sav", to.data.frame=TRUE)
 
-# return dimension of dataset1
-dim(fm1)
 
-
-summary(fm1$nation) # summary of nation
-
-
-summary(fm1$nation)[["american"]] # number of americans
-
-# number of japanese
-summary(fm1$nation)[["japanese"]]
+#-----------------------------FM1---------------------------#
+dim(fm1) 					# return dimension of dataset1
+summary(fm1$nation) 			# summary of nation
+summary(fm1$nation)[["american"]] 	# number of americans
+summary(fm1$nation)[["japanese"]]	# number of japanese
 
 # Welch's two Sample t-test
-d1_hmean_t <- t.test(fm1$h_dialmean ~ fm1$nation)
+t.test(fm1$h_dialmean ~ fm1$nation)
+t.test(fm1$a_dialmean ~ fm1$nation)
+
+
+# Levene-type test
+install.packages("lawstat")
+library("lawstat") 
+levene.test(y= fm1$h_dialmean, group= fm1$nation) 
+levene.test(y= fm1$a_dialmean, group= fm1$nation) 
+
+
+
+
+
 
 d1_hmean_t$p.value
 d1_hmean_t$parameter
 d1_hmean_t$estimate
 
-d1_amean_t <- t.test(dataset1$a_dialmean~dataset1$nation)
-d2_hmean_t <- t.test(dataset2$h_dialmean~dataset2$nation)
-d2_amean_t <- t.test(dataset2$a_dialmean~dataset2$nation)
+d1_amean_t <- t.test(fm1$a_dialmean~fm1$nation)
+d2_hmean_t <- t.test(fm2$h_dialmean~fm2$nation)
+d2_amean_t <- t.test(fm2$a_dialmean~fm2$nation)
