48a6165a56c99448984947d03fdeee82170f35a5
Byungcheon Ko <bcko@umich.edu>
2016-05-19 11:13:24 -0400

    include repeated measure anova and confidence interval

--- script.R
+++ script.R
@@ -25,9 +25,41 @@ levene.test(y= fm1$h_dialmean, group= fm1$nation)
 levene.test(y= fm1$a_dialmean, group= fm1$nation) 
 
 
+# variables into factors
+fm1 <- within(fm1, {
+nation <- factor(nation)
+version <- factor(version)
+gender <- factor(gender)
+})
 
 
+happy_fm1 <-data.frame(fm1$id, fm1$h_dialmean, rep(x="happy",times=150), fm1$nation, fm1$version, fm1$gender)
+colnames(happy_fm1) <- c("id", "dialmean", "face", "nation", "version", "gender")
 
+angry_fm1 <-data.frame(fm1$id, fm1$a_dialmean, rep(x="angry", times=150), fm1$nation, fm1$version, fm1$gender)
+colnames(angry_fm1) <- c("id", "dialmean", "face", "nation", "version", "gender")
+
+dialmean_fm1 <- rbind(happy_fm1, angry_fm1)
+dialmean_fm1$face <- as.factor(dialmean_fm1$face)
+
+
+# before we can run ANOVA, we need to find mean dialmean value for each participant for each combination of condition
+dialmean_fm1.mean <- aggregate(dialmean_fm1$dialmean,
+					by = list(dialmean_fm1$id, dialmean_fm1$face),
+					FUN = 'mean')
+
+colnames(dialmean_fm1.mean) <- c("id", "face", "dialmean")
+head(dialmean_fm1.mean)
+dialmean_fm1.mean <- dialmean_fm1.mean[order(dialmean_fm1.mean$id),]
+
+# building the anova
+
+dialmean.aov <- with(dialmean_fm1.mean, aov(dialmean ~ face) + Error( )
+summary(dialmean.aov)
+
+confint(dialmean.aov)
+
+names(dialmean.aov)
 
 d1_hmean_t$p.value
 d1_hmean_t$parameter
@@ -36,3 +68,4 @@ d1_hmean_t$estimate
 d1_amean_t <- t.test(fm1$a_dialmean~fm1$nation)
 d2_hmean_t <- t.test(fm2$h_dialmean~fm2$nation)
 d2_amean_t <- t.test(fm2$a_dialmean~fm2$nation)
+
